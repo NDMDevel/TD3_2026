99571731e7303c98d6e4107bef7e77b3b7883ce9
NDMDevel <damian.n.marasco@gmail.com>
2026-05-19 19:34:08 -0300

    Update WIFI Server Socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,84 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include "Timers.h"
+#include "checksum.h"
 
-//0- preferir codigo declarativo a codigo imperativo
-//   (declarativo, dice que hace)
-//   (imperativo, dice como lo hace)
-
-//1- hacer interfaces (funciones) faciles de usar CORRECTAMENTE
-//   y dificilies de INCORRECTAMENTE
-
-//2- mostrar gradualmente los detalles de codigo
-
-//3- crear funciones de soporte (que resuelven cosas puntuales/acotadas)
-
-void toggle_pin(int pin)
-{
-    digitalWrite(pin, !digitalRead(pin) );
-}
+WiFiServer server(5000);
+String sta_SSID = "LAB_ELECTRONICA";
+String sta_PASS = "LabElec2022";
 
 Tim32_ms timer;
 
-//C
-//enum Blinky_st
-//{
-//    shutdown = 0,
-//    toggle   = 1,
-//    waiting  = 2
-//};
-
-//C++
-enum class Blinky_st : uint8_t
+uint16_t crc_16(uint8_t *buffer, uint16_t length)
 {
-    shutdown = 0,
-    toggle   = 1,
-    waiting  = 2
-};
-
-enum class Modbus_st
-{
-    shutdown = 0,
-    toggle   = 1,
-    waiting  = 2
-};
-
-Blinky_st st;
-
-void blinky()
-{
-    if( st == Blinky_st::shutdown )
+    uint16_t i, j, temp_bit, temp_int, crc;
+    crc = 0xFFFF;
+    for (i = 0; i < length; i++)
     {
-        return;
+        temp_int = (unsigned char)*buffer++;
+        crc ^= temp_int;
+        for (j = 0; j < 8; j++)
+        {
+            temp_bit = crc & 0x0001;
+            crc >>= 1;
+            if (temp_bit != 0)
+                crc ^= 0xA001;
+        }
     }
-    if( st == Blinky_st::toggle )
-    {
-        toggle_pin(2);
-        timer.start();
-        st = Blinky_st::waiting;
-        return;
-    }
-    if( st == Blinky_st::waiting )
-    {
-        if( timer < 100ms )
-            return;
-        st = Blinky_st::toggle;
-        return;
-    }
+    return crc;
 }
 
 void setup()
 {
-    //pin 2 as output (assume LED)
-    pinMode(2,OUTPUT);
+    Serial.begin(9600);
+    timer.start();
 
-    st = Blinky_st::toggle;
+    WiFi.begin( sta_SSID , sta_PASS );
+
+    Serial.println("Connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+    }
+    Serial.println("\nConnected IP: "+WiFi.localIP().toString());
+
+    server.begin();
+
 }
 
 void loop()
 {
-    blinky();
+    if( timer > 1s )    
+    {
+        uint16_t mb_address = 40030;
+
+        uint8_t mb_frame[64];
+        mb_frame[0] = 0x01;
+        mb_frame[1] = 0x03;
+        mb_frame[2] = mb_address >> 8;
+        mb_frame[3] = mb_address & 0x00FF;
+        mb_frame[4] = 0x00;
+        mb_frame[5] = 0x01;
+        uint16_t crc = crc_modbus( mb_frame , 6 );
+        mb_frame[6] = crc >> 8;
+        mb_frame[7] = crc & 0x00FF;
+
+        //Serial.write( mb_frame , 8);
+
+        timer.start();
+    }
+    WiFiClient client = server.available();
+
+    if( client )
+    {
+        while (client.connected())
+        {
+            while (client.available())
+            {
+                uint8_t b = client.read();
+                Serial.printf("%02X ", b);
+            }
+        }
+        client.stop();
+    }
 }
